6c06b733dfdc476e95376527ea6f60086806bc24
Brian-15 <bcirieco@gmail.com>
2021-07-15 19:12:22 -0700

    - getGiphy: handler function listening for form submission, and passes gif data to appendGif function - appendGif: adds GIF in table. Appends new row when cell count reaches 3. '

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 console.log("Let's get this party started!");
+
+const [submitButton, removeImgsButton] = $('button');
+const form = $('form');
+let currRow = 0;
+
+form.on('submit', getGiphy);
+
+async function getGiphy(evt) {
+
+    evt.preventDefault();
+    
+    // get search term
+    const term = $('#searchTerm')[0].value;
+
+    // get JSON data from Giphy API
+    const result = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${term}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);;
+
+    // generate GIF element and append to table element
+    appendGif(result.data.data[0]);
+}
+
+function appendGif(data) {
+
+    const $row = $(`#${currRow}`);
+
+    // set up GIF element
+    const gif = $('<img>');
+    gif.attr('src', data.url);
+    console.log();
+
+    // set up cell
+    const cell = $('<td>');
+    cell.append(gif);
+    
+    if ($row.children().length === 3) {
+        currRow++;
+    }
+
+    $(`#${currRow}`).append(cell);
+}
